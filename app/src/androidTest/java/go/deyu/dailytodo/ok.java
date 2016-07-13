f25a18e65498b98f0e57ce9fa00d2328d8e2e065
DeyuGoGo <a0924xxx@gmail.com>
2016-07-13 11:47:59 +0800

    [Master] 1.Test

---
+++ app/src/androidTest/java/go/deyu/dailytodo/ok.java
@@ -0,0 +1,12 @@
+package go.deyu.dailytodo;
+
+/**
+ * Created by Deyu on 16/1/27.
+ */
+public enum ok {
+    OK(123),DD(123);
+    public int a ;
+    private ok(int a){
+        this.a = a;
+    }
+}
